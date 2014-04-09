d1786722c732e74ea88ff22aefc69f8ea5deee9a
corsairdnb <dmitry.rychkov@gmail.com>
2014-04-10 01:48:31 +0400

    провал всех попыток сделать поиск пути

--- js/lines.js
+++ js/lines.js
@@ -73,20 +73,16 @@ var Lines = function(settings) {
 
     // get array of random balls
     app.getRandomBalls = function() {
-        var balls = [];
+        var balls = {}, number;
         for (var i = 0; i < params.ballsPerRound; i++) {
-            balls.push({
-                number: (function(){
-                    var num = app.getRandomInt(1, params.cellsCount);
-                    for (var n in balls) {
-                        while (balls[n]['number']==num) {
-                            num = app.getRandomInt(1, params.cellsCount);
-                        }
-                    }
-                    return num;
-                }()),
-                color: app.getRandomColor()
-            });
+            number = (function(){
+                var n = app.getRandomInt(1, params.cellsCount);
+                if (typeof balls[n]==="undefined") {
+                    return n;
+                }
+                i--;
+            }());
+            balls[number] = app.getRandomColor();
         }
         return balls;
     };
@@ -117,89 +113,144 @@ var Lines = function(settings) {
         }
     };
 
+    app.loadFromMap = function() {
+        var map = app.load("map");
+        if (isEmpty(map)) map = false;
+        return map;
+    };
+
+    app.saveMap = function(n,color) {
+        var map = app.load("map") || {};
+        //if (map[n] && typeof map[n]!=="undefined") {
+            map[n] = color;
+        //}
+        app.save("map",map);
+    };
+
+    app.clearHistory = function() {
+        localStorage.removeItem("map");
+    };
+
     // clear container & fill with cells
     app.initGrid = function(){
+        var n=1, balls = app.loadFromMap() || app.load("next") || app.getRandomBalls();
         params.container.innerHTML = "";
-        for (var i=1; i<=params.cellsCount; i++) {
-            $("<div/>")
-                .addClass(params.cellClass)
-                .attr("data-cell",i)
-                .on("click",function(){
-                    app.moveBallTo(this);
-                })
-                .appendTo($(params.container));
+        for (var j=params.size-1; j>=0; j--) {
+            for (var i=0; i<params.size; i++) {
+                $("<div/>")
+                    .addClass(params.cellClass)
+                    .attr("data-cell",n)
+                    .attr("data-x",i)
+                    .attr("data-y",j)
+                    .html((function(){
+                        if (n in balls && balls[n]) {
+                            params.map[j][i] = -1;
+                            app.saveMap(n,balls[n]);
+                            return $("<div/>").addClass(params.ballClass+" "+params.ballClass+"-"+balls[n]).attr("data-color",balls[n]);
+                        }
+                    }()))
+                    .on("click",function(){
+                        app.selectCell(this);
+                        app.moveBallTo(this);
+
+                    })
+                    .appendTo($(params.container));
+                n++;
+            }
         }
     };
 
     // append random balls to grid
     app.spawnBalls = function() {
-        var ballsData = app.load("next") || app.getRandomBalls();
-        for (var i in ballsData) {
-            var cell = $("[data-cell='"+ballsData[i]['number']+"']")
+        var balls = app.load("next") || app.getRandomBalls();
+        for (var i in balls) {
+            var cell = $("[data-cell='"+i+"']")
                 .html("")
                 .on("click",function(){
-                    app.selectBall(this);
+
+                    app.selectCell(this);
                 });
-            $("<div/>").addClass(params.ballClass+" "+params.ballClass+"-"+ballsData[i]['color']).appendTo(cell);
-            var coordinates = app.getCoordinates(ballsData[i]['number']);
+            app.saveMap(i,balls[i]);
+            $("<div/>").addClass(params.ballClass+" "+params.ballClass+"-"+balls[i]).attr("data-color",balls[i]).appendTo(cell);
+            var coordinates = app.getCoordinates(cell);
             params.map[coordinates[1]][coordinates[0]] = -1;
         }
+        //console.log(JSON.stringify(params.map));
+        return false;
     };
 
     // get array coordinates by number of ball
-    app.getCoordinates = function(n) {
+    app.getCoordinates = function(cell) {
         return [
-            (function(){
-                if (n % params.size!=0) return parseInt((n / params.size).toString().charAt(2))-1;
-                else return params.size-1;
-            }()), // x
-            params.size - Math.ceil(n/params.size-1) - 1 // y
+            parseInt($(cell).attr("data-x")), // x
+            parseInt($(cell).attr("data-y")) // y
         ];
     };
 
-    // get number
-    app.getCellNumber = function(coords) {
-
-    };
-
     // select ball on click
-    app.selectBall = function(cell) {
-        var cells = $("."+params.cellClass);
-        cells.not($(cell)).removeClass(params.cellSelectedClass);
-        if (!$(cell).hasClass(params.cellSelectedClass))
-            $(cell).addClass(params.cellSelectedClass);
+    app.selectCell = function(cell) {
+        var cells = $("."+params.cellClass), $cell = $(cell);
+        if (!$cell.hasClass(params.cellSelectedClass)) {
+            if ($cell.children().length) {
+                cells.removeClass(params.cellSelectedClass);
+                $cell.addClass(params.cellSelectedClass);
+            }
+        }
         else
-            $(cell).removeClass(params.cellSelectedClass);
+            $cell.removeClass(params.cellSelectedClass);
     };
 
     // try to move ball to target cell
     app.moveBallTo = function(target) {
-        var selectedCell = document.querySelectorAll("."+params.cellClass+"."+params.cellSelectedClass);
-        if ($(target).hasClass(params.cellSelectedClass) || selectedCell.length==0){
+        var selectedCell = document.querySelectorAll("."+params.cellClass+"."+params.cellSelectedClass),
+            $target = $(target);
+        if ($target.hasClass(params.cellSelectedClass) || $target.children().length || selectedCell.length==0){
             return false; // return if no need to work
         }
 
-        var testMap = params.map,
-            targetCellNumber = $(target).attr("data-cell"),
-            selectedCellNumber = $(selectedCell).attr("data-cell"),
-            targetCoordinates = app.getCoordinates(parseInt(targetCellNumber)),
-            selectedCoordinates = app.getCoordinates(parseInt(selectedCellNumber));
+        var testMap = (function(){
+                var ar = [];
+                for (var i=0; i<params.size; i++) {
+                    ar[i] = [];
+                    for (var j=0; j<params.size; j++) {
+                        ar[i][j] = params.map[i][j];
+                    }
+                }
+                return ar;
+            }()),
+            //targetCellNumber = $target.attr("data-cell"),
+            //selectedCellNumber = $(selectedCell).attr("data-cell"),
+            targetCoordinates = app.getCoordinates(target),
+            selectedCoordinates = app.getCoordinates(selectedCell),
+            trace;
 
         testMap = testMap.reverse();
         //console.log(JSON.stringify(testMap));
         //console.log(selectedCoordinates[0], selectedCoordinates[1], targetCoordinates[0], targetCoordinates[1]);
 
-        var trace = app.pathFind(selectedCoordinates[0], selectedCoordinates[1], targetCoordinates[0], targetCoordinates[1], testMap);
+        trace = app.pathFind(selectedCoordinates[0], selectedCoordinates[1], targetCoordinates[0], targetCoordinates[1], testMap);
         if (trace) {
-            console.log(trace);
             app.pathTrace(trace);
         }
-
+        return false;
     };
 
     // trace path
     app.pathTrace = function(trace) {
-
+        //console.log(trace);
+        var coord, cell, n=0;
+        for (var i in trace) {
+            coord = trace[i];
+            cell = $("[data-x='"+coord[0]+"'][data-y='"+coord[1]+"']");
+            cell.addClass("traced");
+            setTimeout(function(cell){
+                cell.removeClass("traced");
+            },150*n,cell);
+            n++;
+        }
+        app.spawnBalls();
+        app.prepareNextBalls();
+        return false;
     };
 
     // find path and get coordinates for tracing
@@ -211,70 +262,86 @@ var Lines = function(settings) {
             x, y, d,
             dx = [0, 0, -1, 1],
             dy = [-1, 1, 0, 0],
-            //blackList = [],
             path = [],
             pathLength,
+            blackList = (function(){
+                var ar = [];
+                for (var i=0; i<params.size; i++) {
+                    ar[i] = [];
+                    for (var j=0; j<params.size; j++) {
+                        ar[i][j] = -2;
+                    }
+                }
+                return ar;
+            }()),
             searchComplete;
 
-        map[ay][ax] = 0; // start point
+        map[by][bx] = 0; // start searching from final point
+        blackList[by][bx] = 0; // start searching from final point
+        //console.log(ax, ay, bx, by);
         do {
             searchComplete = true;
-            for (y=0; y<size; y++) {
-                for (x=0; x<size; x++) {
-                    if (map[y][x]==wave/* && map[y][x]!=wall*/) {
+            for (y=size-1; y>=0; y--) {
+                for (x=size-1; x>=0; x--) {
+                    if (map[y][x]==wave) {
                         // check neighbours
                         for (d=0; d<4; d++) {
-                            // if cell exists & is not wall & not in blacklist
-
-                            if (y+dy[d] in map && x+dx[d] in map[y+dy[d]] && map[y+dy[d]][x+dx[d]] == blank) {
+                            // if cell exists & is not wall
+                            if (y+dy[d] in map && x+dx[d] in map[y+dy[d]]) {
                                 // there was at least one not checked cell
                                 // that cell will generate next wave
-                                searchComplete = false;
-                                map[y+dy[d]][x+dx[d]] = wave++;
+                                if (map[y+dy[d]][x+dx[d]] == blank) {
+                                    searchComplete = false;
+                                    map[y+dy[d]][x+dx[d]] = wave + 1;
+                                }
+                                /*else if (map[y+dy[d]][x+dx[d]] == wall) {
+                                    blackList[y+dy[d]][x+dx[d]] = wall;
+                                    $("[data-x='"+eval(y+dy[d])+"'][data-y='"+eval(x+dx[d])+"']").css("background","black");
+                                }*/
                             }
                         }
                     }
                 }
             }
             wave++;
-            if (by == y+dy[d] && bx ==x+dx[d]) searchComplete = true;
         }
-        // there are not checked cells, final cell is not reached and program has not fell in endless recursion :)
-        while (!searchComplete && map[by][bx]==blank);
+        // there are not checked cells, start cell is not reached and program has not fell in endless recursion :)
+        while (!searchComplete && map[ay][ax]==blank);
 
         // if final cell has not been reached - there is no way
-        if (map[by][bx] == blank) return false;
+        if (map[ay][ax] == blank) return false;
 
-        pathLength = map[by][bx];
-        x = bx;
-        y = by;
+        pathLength = map[ay][ax];
+
+        x = ax;
+        y = ay;
         // go back to start point and get coordinates for tracing
         while (pathLength > 0) {
             path.push([x, y]);
             pathLength--;
             for (d=0; d<4; d++) {
                 // if this cell is closer to start
-                if (y+dy[d] in map && x+dx[d] in map[y+dy[d]] && map[y+dy[d]][x+dx[d]] == pathLength) {
+                if (y+dy[d] in map && x+dx[d] in map[y+dy[d]] && blackList[y+dy[d]][x+dx[d]] != -1 && map[y+dy[d]][x+dx[d]] == pathLength) {
                     x = x+dx[d];
                     y = y+dy[d];
                     break;
                 }
             }
         }
-        path.push([ax,ay]); // last point is start
-        return path.reverse();
+        path.push([bx,by]); // last point is start
+        return path;
     };
 
     // append random balls to grid
     app.prepareNextBalls = function() {
-        var ballsData = app.getRandomBalls();
+        var balls = app.getRandomBalls();
         params.queue.innerHTML = "";
-        for (var i in ballsData) {
+        for (var i in balls) {
             $("<div/>")
-                .addClass(params.ballClass+"-queue "+params.ballClass+"-"+ballsData[i]['color'])
+                .addClass(params.ballClass+"-queue "+params.ballClass+"-"+balls[i])
                 .appendTo($(params.queue));
         }
-        app.save("next", ballsData);
+        app.save("next", balls);
     };
 
     // set ready state
@@ -292,7 +359,7 @@ var Lines = function(settings) {
     // start new game
     app.start = function() {
         app.initGrid();
-        app.spawnBalls();
+        //app.spawnBalls();
         app.prepareNextBalls();
         setTimeout(function(){
             app.ready();
@@ -302,9 +369,10 @@ var Lines = function(settings) {
     // restart game
     app.restart = function() {
         app.ready(false);
+        app.clearHistory();
         setTimeout(function(){
             app.initGrid();
-            app.spawnBalls();
+            //app.spawnBalls();
             app.prepareNextBalls();
             setTimeout(function(){
                 app.ready();
@@ -326,4 +394,13 @@ function extend(o, p) {
         o[prop] = p[prop];
     }
     return o;
+}
+
+// check if object is empty
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
 }
